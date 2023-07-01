da972b1a1c36fd2c0ccb902a9be8279f2a90da2c
João Manoel Carvalho <joaomanoel190701@gmail.com>
2023-07-01 12:14:24 -0300

    codigo com grafo

--- prova2.py
+++ prova2.py
@@ -1,106 +1,144 @@
 import tkinter as tk
-from tkinter import CENTER, messagebox
+from tkinter import messagebox
+import networkx as nx
+import matplotlib.pyplot as plt
 
-class GraphApp:
+
+class GraphInputApp:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("Árvore Geradora Mínima")
+        self.root.title("Inserir Dados do Grafo")
+        self.center_window(self.root, 400, 200)  # Centralizar a janela
 
-        # Definir o tamanho da janela
-        width = 400
-        height = 800
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        x = (screen_width - width) // 2
-        y = (screen_height - height) // 2
-        self.root.geometry(f"{width}x{height}+{x}+{y}")
-
-        self.canvas = tk.Canvas(self.root, width=400, height=300)
-        self.canvas.pack()
         self.num_vertices_label = tk.Label(self.root, text="Número de Vértices:")
         self.num_vertices_label.pack()
-        self.num_vertices_entry = tk.Entry(self.root)
+        self.num_vertices_entry = tk.Entry(self.root, justify="center")  # Centralizar texto
         self.num_vertices_entry.pack()
         self.num_arestas_label = tk.Label(self.root, text="Número de Arestas:")
         self.num_arestas_label.pack()
-        self.num_arestas_entry = tk.Entry(self.root)
+        self.num_arestas_entry = tk.Entry(self.root, justify="center")  # Centralizar texto
         self.num_arestas_entry.pack()
         self.create_graph_button = tk.Button(self.root, text="Criar Grafo", command=self.create_graph)
         self.create_graph_button.pack()
-        self.restart_button = tk.Button(self.root, text="Reiniciar", state=tk.DISABLED, command=self.restart)
-        self.restart_button.pack()
+
         self.vertices = 0
         self.arestas = 0
-        self.grafo = None
-        self.index_aresta = 1
-        self.aresta_entries = []
+        self.graph_data = []
+
+    def center_window(self, window, width, height):
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+        window.geometry(f"{width}x{height}+{x}+{y}")
 
     def create_graph(self):
-        self.vertices = int(self.num_vertices_entry.get())
-        self.arestas = int(self.num_arestas_entry.get())
-        self.canvas.delete("all")
-        self.grafo = Grafo(self.vertices)
-        self.add_aresta_entry()
+        num_vertices = self.num_vertices_entry.get()
+        num_arestas = self.num_arestas_entry.get()
+        if not num_vertices.isdigit() or not num_arestas.isdigit():
+            tk.messagebox.showerror("Erro", "Insira um número válido para o número de vértices e arestas.")
+            return
 
-    def add_aresta_entry(self):
-        if self.index_aresta <= self.arestas:
-            aresta_label = tk.Label(self.root, text="Aresta {}: ".format(self.index_aresta))
-            aresta_label.pack()
-            aresta_entry = tk.Entry(self.root)
-            aresta_entry.pack()
-            aresta_entry.bind("<Return>", self.add_aresta)
-            aresta_entry.focus_set()
-            self.aresta_entries.append(aresta_entry)
+        self.vertices = int(num_vertices)
+        self.arestas = int(num_arestas)
+        self.root.withdraw()  # Esconder a tela de entrada de dados
+        self.graph_input = GraphDataInputApp(self.vertices, self.arestas, self.graph_data, self.create_new_graph)
 
-    def add_aresta(self, event):
-        aresta = event.widget.get()
-        u, v, peso = map(int, aresta.split())
-        self.grafo.adicionar_aresta(u-1, v-1, peso)
-        self.index_aresta += 1
-        event.widget.destroy()
-        if self.index_aresta > self.arestas:
-            arvore_geradora = self.grafo.kruskal()
-            self.mostrar_arvore_geradora(arvore_geradora)
-            self.restart_button.config(state=tk.NORMAL)
-        else:
-            self.add_aresta_entry()
+    def create_new_graph(self):
+        self.root.deiconify()  # Mostrar novamente a tela de entrada de dados
+        self.graph_data = []  # Limpar os dados do grafo anterior
+        self.num_vertices_entry.delete(0, tk.END)  # Limpar o campo de entrada de número de vértices
+        self.num_arestas_entry.delete(0, tk.END)  # Limpar o campo de entrada de número de arestas
 
-    def mostrar_arvore_geradora(self, arvore_geradora):
-        self.canvas.delete("all")
-        self.canvas.create_text(200, 50, text="Árvore Geradora Mínima:", font=("Arial", 16), fill="black", justify=CENTER)
-        y = 100
-        for u, v, peso in arvore_geradora:
-            texto = "{} - {} : {}".format(u+1, v+1, peso)
-            self.canvas.create_text(200, y, text=texto, font=("Arial", 12), fill="black", justify=CENTER)
-            y += 20
 
-    def restart(self):
-        self.num_vertices_entry.delete(0, tk.END)
-        self.num_arestas_entry.delete(0, tk.END)
-        self.create_graph_button.config(state=tk.NORMAL)
-        self.restart_button.config(state=tk.DISABLED)
-        self.canvas.delete("all")
-        self.index_aresta = 1
-        for entry in self.aresta_entries:
-            entry.destroy()
+class GraphDataInputApp:
+    def __init__(self, vertices, arestas, graph_data, create_new_graph_callback):
+        self.root = tk.Toplevel()
+        self.root.title("Inserir Dados das Arestas")
+        self.center_window(self.root, 700, 400)  # Centralizar a janela
+
+        self.vertices = vertices
+        self.arestas = arestas
+        self.graph_data = graph_data
+        self.create_new_graph_callback = create_new_graph_callback
+
+        self.aresta_label = tk.Label(self.root, text="Insira os dados das arestas:")
+        self.aresta_label.pack()
         self.aresta_entries = []
+        for i in range(self.arestas):
+            frame = tk.Frame(self.root)
+            frame.pack()
+            u_label = tk.Label(frame, text="Vértice u:")
+            u_label.pack(side=tk.LEFT)
+            u_entry = tk.Entry(frame, justify="center")  # Centralizar texto
+            u_entry.pack(side=tk.LEFT)
+            v_label = tk.Label(frame, text="Vértice v:")
+            v_label.pack(side=tk.LEFT)
+            v_entry = tk.Entry(frame, justify="center")  # Centralizar texto
+            v_entry.pack(side=tk.LEFT)
+            peso_label = tk.Label(frame, text="Peso:")
+            peso_label.pack(side=tk.LEFT)
+            peso_entry = tk.Entry(frame, justify="center")  # Centralizar texto
+            peso_entry.pack(side=tk.LEFT)
+            self.aresta_entries.append((u_entry, v_entry, peso_entry))
 
-    def run(self):
-        self.root.mainloop()
+        self.confirm_button = tk.Button(self.root, text="Confirmar", command=self.confirm_graph)
+        self.confirm_button.pack()
+
+    def center_window(self, window, width, height):
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+        window.geometry(f"{width}x{height}+{x}+{y}")
+
+    def confirm_graph(self):
+        for u_entry, v_entry, peso_entry in self.aresta_entries:
+            u = int(u_entry.get())
+            v = int(v_entry.get())
+            peso = float(peso_entry.get())
+            self.graph_data.append((u, v, peso))
+        self.root.destroy()  # Fechar a tela de entrada de dados
+        self.show_graph()
+
+    def show_graph(self):
+        grafo = Grafo(self.vertices)
+        for u, v, peso in self.graph_data:
+            grafo.adicionar_aresta(u, v, peso)
+
+        arvore_geradora = grafo.kruskal()
+
+        graph = nx.Graph()
+        graph.add_edges_from([(u, v, {'weight': peso}) for u, v, peso in grafo.grafo.edges(data='weight')])
+
+        pos = nx.spring_layout(graph)
+
+        plt.figure(figsize=(8, 6))
+        nx.draw_networkx(graph, pos=pos, with_labels=True, node_color='lightgreen', node_size=500)
+        nx.draw_networkx_edges(graph, pos=pos, edgelist=arvore_geradora, edge_color='red', width=2)
+        labels = {(u, v): f"{peso:.2f}" for (u, v, peso) in graph.edges(data='weight')}
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels, font_color='#000000', font_size=10)
+        plt.axis('off')
+        plt.show()
+
+        self.create_new_graph_callback()
 
 
 class Grafo:
     def __init__(self, vertices):
         self.V = vertices
-        self.grafo = []
+        self.grafo = nx.Graph()
 
     def adicionar_aresta(self, u, v, peso):
-        self.grafo.append((u, v, peso))
+        self.grafo.add_edge(u, v, weight=peso)
 
     def encontrar(self, pai, i):
-        if pai[i] == i:
-            return i
-        return self.encontrar(pai, pai[i])
+        if i >= len(pai):
+            return -1  # Retorna -1 se o índice estiver fora do intervalo
+        while pai[i] != i:
+            i = pai[i]
+        return i
+
 
     def unir(self, pai, rank, x, y):
         raiz_x = self.encontrar(pai, x)
@@ -115,25 +153,24 @@ class Grafo:
 
     def kruskal(self):
         arvore_geradora = []
-        self.grafo = sorted(self.grafo, key=lambda item: item[2])
+        i = 0
+        arestas = sorted(self.grafo.edges(data='weight'), key=lambda aresta: aresta[2])
         pai = []
         rank = []
-        for v in range(self.V):
-            pai.append(v)
+        for vertice in range(self.V):
+            pai.append(vertice)
             rank.append(0)
-        i = 0
-        arestas_selecionadas = 0
-        while arestas_selecionadas < self.V - 1:
-            u, v, peso = self.grafo[i]
+        while len(arvore_geradora) < self.V - 1 and i < len(arestas):
+            u, v, peso = arestas[i]
             i += 1
-            raiz_u = self.encontrar(pai, u)
-            raiz_v = self.encontrar(pai, v)
-            if raiz_u != raiz_v:
-                arvore_geradora.append((u, v, peso))
-                self.unir(pai, rank, raiz_u, raiz_v)
-                arestas_selecionadas += 1
+            x = self.encontrar(pai, u)
+            y = self.encontrar(pai, v)
+            if x != y:
+                arvore_geradora.append((u, v))
+                self.unir(pai, rank, x, y)
         return arvore_geradora
 
 
-app = GraphApp()
-app.run()
+if __name__ == "__main__":
+    app = GraphInputApp()
+    app.root.mainloop()
